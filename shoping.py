38e1b714a5a98e28edf394655895a216a096902c
iamhacker2367-sudo <iamhacker2367@gmail.com>
2025-08-26 20:15:27 +0530

    Add files via upload

---
+++ shoping.py
@@ -0,0 +1,14 @@
+Shoping_list=[]
+item1=input()
+item2=input()
+item3=input()
+item4=input()
+item5=input()
+Shoping_list.append(item1)
+Shoping_list.append(item2)
+Shoping_list.append(item3)
+Shoping_list.append(item4)
+Shoping_list.append(item5)
+st=input()
+if st=="yu":
+    print(Shoping_list)
